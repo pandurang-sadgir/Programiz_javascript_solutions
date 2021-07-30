d8a013ff39f5499eeacc153573f9fdf70ace74c8
pandurang sadgir <psadgir111@gmail.com>
2021-07-30 17:58:44 +0530

    square root of number using Math.sqrt() method

---
+++ square-root.js
@@ -0,0 +1,19 @@
+"use strict"
+/*
+const number = Number( prompt('Enter number that want square root '));
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
+console.log(typeof result);
+
+*/
+
+// Note: Math.sqrt() method return square root of positive number only
+let number1 = 9;
+let number2 = 'sdg';
+let number3 = -4;
+let sqrtOfNumber1 = Math.sqrt(number1);
+let sqrtOfNumber2 = Math.sqrt(number2);
+let sqrtOfNumber3 = Math.sqrt(number3);
+console.log(`The square of ${number1} is ${sqrtOfNumber1} And its type is ${typeof number1}`);
+console.log(`The square of ${number2} is ${sqrtOfNumber2} And its type is ${typeof number2}`);
+console.log(`The square of ${number3} is ${sqrtOfNumber3} And its type is ${typeof number3}`);
